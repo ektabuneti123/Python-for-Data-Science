7afd28c04048f81bae496bf36322f822cb101044
ektabuneti123 <bunetikrishna@gmail.com>
2026-07-30 13:04:58 +0530

    Add files via upload

---
+++ Practical5a.py
@@ -0,0 +1,12 @@
+#EktaS078
+# Practical 5(a)
+# Importing pandas library and creating a DataFrame object
+
+import pandas as pd
+
+# Read the Excel dataset
+df = pd.read_excel("StressLevelDataset.xlsx")
+
+# Display the DataFrame
+print("DataFrame:")
+print(df)
